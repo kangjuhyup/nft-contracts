9dfb9f124455c5c1d70ae152358d47a9f90163d0
mantisco-kangjuhyup <fog0510@gmail.com>
2023-06-09 23:22:06 +0900

    fix erc20deployer

--- contracts/ERC20Deployer.sol
+++ contracts/ERC20Deployer.sol
@@ -1,23 +1,34 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import "./ERC20Template.sol";
-
 
 contract ERC20Deployer {
 
-    event DeployERC20Event(address indexed owner, address indexed token);
+  event DeployERC20Event(address indexed owner, address indexed erc20);
 
-    mapping(address => address[]) deployed;
+  mapping(address => address[]) deployed;
 
-    function DeployERC20(string memory name, string memory symbol, uint256 totalSupply) public returns (address){
-        address contract_address = address(new ERC20Template(name,symbol,totalSupply,msg.sender));
-        deployed[msg.sender].push(contract_address);
-        emit DeployERC20Event(msg.sender, contract_address);
-        return contract_address;
+  function deployERC20(bytes memory bytecode) public {
+    bytes memory data = abi.encodeWithSignature("transferOwnership(address)", msg.sender);
+    address contractB;
+    assembly {
+      contractB := create(0, add(bytecode, 0x20), mload(bytecode))
+      if iszero(contractB) {
+          returndatacopy(0, 0, returndatasize())
+          revert(0, returndatasize())
+      }
+
+      let success := call(gas(), contractB, 0, add(data, 0x20), mload(data), 0, 0)
+      if iszero(success) {
+        returndatacopy(0, 0, returndatasize())
+        revert(0, returndatasize())
+      }
     }
+    deployed[msg.sender].push(contractB);
+    emit DeployERC20Event(msg.sender, contractB);
+  }
 
-    function getERC20(address owner) public view returns (address[] memory) {
-        return deployed[owner];
-    }
+  function getERC20(address owner) public view returns (address[] memory) {
+    return deployed[owner];
+  }
 }
